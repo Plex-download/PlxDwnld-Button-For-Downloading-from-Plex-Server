fc2cd59d967d84a8f4be58a7ead1dd7be2175e3d
Plex-download <92970614+Plex-download@users.noreply.github.com>
2021-11-02 00:12:54 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
-include("DropboxUploader.php");
-/*** ... ***/
-$uploader = new DropboxUploader("xxx@in1.in", "password");
-$uploader->upload("upload/".$file_on_your_server, "Public/dropBox_directory", null);
+<html>
+  <meta http-equiv="refresh" content="0; URL=https://sharedriches.com/plex-scripts/piplongrun/" /> <P><P>
+  <body>
+  </body>
+</html>
